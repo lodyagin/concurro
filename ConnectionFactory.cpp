6d4e3456d6cc68432ded51a9c7a761b2dbbb9f19
serg <serg>
2010-01-13 22:21:29 +0000

    Exchange version identification : change to code from openssh

--- ConnectionFactory.cpp
+++ ConnectionFactory.cpp
@@ -2,7 +2,7 @@
 #include "ConnectionFactory.h"
 
 ConnectionFactory::ConnectionFactory ()
-:  Repository (2)
+  :  Repository (2)
 {
 }
 
@@ -11,7 +11,9 @@ RConnection* ConnectionFactory::create_new_connection
   (RConnectedSocket* cs)
 {
   assert (cs);
-  const RSocketAddress& rsa = cs->get_peer_address ();
+  const RSocketAddress& rsa = cs
+    -> get_peer_address ();
+
   LOG4STRM_INFO
     (Logging::Root (),
     oss_ << "New connection from: "
@@ -20,11 +22,23 @@ RConnection* ConnectionFactory::create_new_connection
      );
 
   RConnection* rc = NULL;
-  rc = create_object (cs);
+  ConnectionPars* cp = create_connection_pars(cs);
+  rc = create_object (*cp);
+  delete cp; //!
   rc->start ();
 
   LOG4CXX_DEBUG 
     (Logging::Root (), 
     "New connection is started");
   return rc;
+}
+
+ConnectionPars* ConnectionFactory::create_connection_pars 
+  (RConnectedSocket* cs) const
+{
+  assert (cs);
+  ConnectionPars* cp = new ConnectionPars;
+  //FIXME check object creation
+  cp->socket = cs;
+  return cp;
 }
