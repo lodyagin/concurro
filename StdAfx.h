7b1687f1df1d6c12f82736456bf14150a99f9590
serg <serg>
2010-03-10 15:07:11 +0000

    Set the target windows version

--- StdAfx.h
+++ StdAfx.h
@@ -1,4 +1,11 @@
 #pragma once
+#include "targetver.h"
+
+#define WIN32_LEAN_AND_MEAN // without winsock 1
+#include <windows.h>
+#include <Ws2tcpip.h>
+#include <Winsock2.h>
+
 #include "Logging.h"
 #include "SWinCheck.h"
 #include "SException.h"
@@ -8,7 +15,7 @@
 #include "SEvent.h"
 #include "SMutex.h"
 #include "SShutdown.h"
+
 #include <string>
 #include <assert.h>
 
-//#include <Winsock2.h>
