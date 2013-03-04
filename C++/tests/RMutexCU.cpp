0d60dc7f43008ffe538b23f48f2a3bfc56a5a2ef
boris <boris@kogorta.dp.ua>
2013-03-04 20:49:09 +0200

    ticket:13 add test for RMutex for 2 threads

--- C++/tests/RMutexCU.cpp
+++ C++/tests/RMutexCU.cpp
@@ -1,6 +1,7 @@
 #include "RMutex.h"
 #include "CUnit.h"
-
+#include "pthread.h"
+#include <iostream>
 //#include <iostream>
 //#include "concurrent/C++/REvent.h"
 //#include <boost/thread/thread.hpp>
@@ -10,12 +11,15 @@
 
 void test_same_thread_acquire();
 void test_same_thread_overrelease();
+void test_2_threads_try_acquire();
 
 CU_TestInfo RMutexTests[] = {
 	{"RMutex can be acquired by the same thread several times",
 	 test_same_thread_acquire },
-	{"RMutex more releases than acquire in the same thread",
-	 test_same_thread_overrelease },
+	 {"RMutex acquire in 1st thread and 2nd thread can't acquire it at same time",
+	 	 test_2_threads_try_acquire },
+	 	{"RMutex more releases than acquire in the same thread",
+	 		 test_same_thread_overrelease },
 	CU_TEST_INFO_NULL
 };
 
@@ -50,5 +54,25 @@ void test_same_thread_overrelease()
   mx.release();
   mx.release();
   mx.release();
+}
+RMutex mx1;
+void* threadFunc(void *arg){
+	mx1.acquire();
+	static volatile int test = 0;
+	sleep(*(int *)arg);
+	*(int*)arg = ++test;
+	mx1.release();
+}
+void test_2_threads_try_acquire(){
+	pthread_t thread1, thread2;
+	int id1 = 2, id2 = 0;
+	pthread_create(&thread1, NULL, threadFunc, &id1);
+	sleep(1);
+	pthread_create(&thread2, NULL, threadFunc, &id2);
+	sleep(3);
+	if (id1 == 1 && id2 == 2)
+		CU_PASS(id1 == 1)
+	else
+		CU_FAIL(id1! = 1);
 }
 
