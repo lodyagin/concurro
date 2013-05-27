af1277418c665333da1ac1e554af1003f76ebd11
boris <boris@kogorta.dp.ua>
2013-05-27 19:45:13 +0300

    ticket:92 toString now use lexical_cast

--- C++/SCommon.h
+++ C++/SCommon.h
@@ -181,17 +181,13 @@ inline const char * ptr2szptr( const T & s )
 template <class T>
 void toString (const T& object, std::string & s)
 {
-  std::ostringstream os;
-  os << object;
-  s += os.str();
+  s = boost::lexical_cast<std::string>(object);
 }
 
 template <class T>
 std::string toString (const T& object)
 {
-  std::ostringstream os;
-  os << object;
-  return os.str();
+  return boost::lexical_cast<std::string>(object);
 }
 
 // FIXME raise exception when the string is not a number
